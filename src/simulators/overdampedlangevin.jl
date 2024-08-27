7511e6459bcc14e502e73029d221cab3d5668596
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-08-27 11:22:15 -0400

    updating OverdampedLangevin simulator to be consistent with new Molly version

--- src/simulators/overdampedlangevin.jl
+++ src/simulators/overdampedlangevin.jl
@@ -7,26 +7,45 @@ function simulation_step!(sys::System,
     run_loggers=true,
     rng=Random.GLOBAL_RNG)
 
-    accels_t = accelerations(sys, neighbors; n_threads=n_threads)
+    forces_t = forces(sys, neighbors; n_threads=n_threads)
+    accels_t = forces_t ./ masses(sys)
 
-    old_coords = copy(sys.coords)
     noise = random_velocities(sys, sim.temperature; rng=rng)
     sys.coords += (accels_t ./ sim.friction) .* sim.dt .+ sqrt((2 / sim.friction) * sim.dt) .* noise
-
-    apply_constraints!(sys, old_coords, sim.dt)
     sys.coords = wrap_coords.(sys.coords, (sys.boundary,))
+
     if !iszero(sim.remove_CM_motion) && step_n % sim.remove_CM_motion == 0
         remove_CM_motion!(sys)
     end
 
     neighbors = find_neighbors(sys, sys.neighbor_finder, neighbors, step_n;
-                                n_threads=n_threads)
+                       n_threads=n_threads)
 
-    run_loggers!(sys, neighbors, step_n, run_loggers; n_threads=n_threads)
+    run_loggers!(sys, neighbors, step_n, run_loggers; n_threads=n_threads,
+                current_forces=forces_t)
 
     return neighbors
 end
 
+# initialize simulation
+function initialize_sim!(sys::System,
+                         sim::OverdampedLangevin;
+                         n_threads::Integer=Threads.nthreads(),
+                         run_loggers=true,
+                        )
+
+    if length(sys.constraints) > 0
+        @warn "OverdampedLangevin is not currently compatible with constraints, " *
+        "constraints will be ignored"
+    end
+
+    sys.coords = wrap_coords.(sys.coords, (sys.boundary,))
+    !iszero(sim.remove_CM_motion) && remove_CM_motion!(sys)
+    nb = find_neighbors(sys, sys.neighbor_finder; n_threads=n_threads)
+    run_loggers!(sys, nb, 0, run_loggers; n_threads=n_threads)
+
+    nb
+end
 
 # function simulate!(sys::System,
 #     sim::OverdampedLangevin,
